37c1c5a79d7464bb80c1e7fbfa8e9fb9a7379309
leslieB-hub <leslie.bileckot@gmail.com>
2021-04-18 16:19:13 +0200

    add login and register page

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,19 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+use App\Security\AppAuthenticator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/registration", name="app_registration")
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param GuardAuthenticatorHandler $guardHandler
+     * @param AppAuthenticator $authenticator
+     * @return Response
      */
-    public function register(): Response
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder,
+                             GuardAuthenticatorHandler $guardHandler, AppAuthenticator $authenticator): Response
     {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user, $form->get('password')->getData())
+            );
+
+
+            $user->setRoles(["ROLE_USER"]);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
+        }
+
         return $this->render('registration/register.html.twig', [
-            'controller_name' => 'RegistrationController',
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -4,8 +4,14 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationType extends AbstractType
 {
@@ -13,14 +19,20 @@ class RegistrationType extends AbstractType
     {
         $builder
             ->add('username',null, ['label'=>'Pseudo'])
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')
-            ->add('telephone')
-            ->add('street')
-            ->add('postcode')
-            ->add('city')
+            ->add('firstname', TextType::class, ['label'=> 'Prénom'])
+            ->add('lastname', TextType::class, ['label'=> 'Nom'])
+            ->add('email', EmailType::class, ['label'=>'Email'])
+            ->add('telephone', TextType::class, ['label'=>'Téléphone'])
+            ->add('street', TextType::class, ['label'=>'Rue'])
+            ->add('postcode', TextType::class, ['label'=>'Code postal'])
+            ->add('city', TextType::class, ['label'=>'Ville'])
+            ->add('password', RepeatedType::class, [
+                'type'=>PasswordType::class,
+                'first_options'=>['label'=>'Mot de passe'],
+                'second_options'=>['label'=>'Confirmation'],
+                'required'=>true,
+                'invalid_message'=>'les mots de passe doivent être identiques'
+            ])
         ;
     }
 
